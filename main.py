a1858c23fea422fba64f822d86b677231823b7b6
Newf <ian@kellys.me>
2024-06-12 16:36:38 -0700

    initial stuff

---
+++ main.py
@@ -0,0 +1,52 @@
+import requests
+
+country = "epg"
+xml = f"https://i.mjh.nz/Roku/{country}.xml"
+output_xml = "rtv.xml"
+    
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
+    
+
+country = "all"
+xml = f"https://i.mjh.nz/PlutoTV/{country}.xml"
+output_xml = "ptv.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
+
+xml = f"https://tinyurl.com/newf276"
+output_xml = "ourguide.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
