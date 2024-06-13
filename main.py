44da0033a11894b727f05c439471748ac4b706ec
Newf <ian@kellys.me>
2024-06-13 16:44:41 -0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import zipfile
 
 country = "epg"
 xml = f"https://i.mjh.nz/Roku/{country}.xml"
@@ -36,38 +35,24 @@ try:
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
 
-xml = f"https://tinyurl.com/newf276"
-output_xml = "ourguide.xml"
-
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
     
-xml = f"https://xmltv.tvkaista.net/guides/ontvtonight.com_ca.xml"
-output_xml = "ontvtonight.xml"
+#xml = f"https://tinyurl.com/newf276"
+#output_xml = "ourguide.xml"
 
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
+#try:
+  #  response = requests.get(xml)
+  #  if response.status_code == 200:
+   #     print("GET request successful.")
 
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
+    #    with open(output_xml, "w") as f:
+     #       f.write(response.text)
+    #        print(f"Response saved to {output_xml}")
+   # else:
+    #    print(f"Failed to retrieve data. Status code: {response.status_code}")
+#except requests.exceptions.RequestException as e:
+    #print("Error making GET request:", e)
 
+    
 xml = f"https://xmltv.tvkaista.net/guides/ontvtonight.com_us.xml"
 output_xml = "ontvtonight_us.xml"
 
@@ -83,6 +68,7 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
+
     
 xml = f"https://xmltv.tvkaista.net/guides/firstmedia.com.xml"
 output_xml = "firstmedia.xml"
@@ -99,6 +85,7 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
+
     
 xml = f"https://xmltv.tvkaista.net/guides/tvhebdo.com.xml"
 output_xml = "tvhebdo.xml"
@@ -115,6 +102,7 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
+
     
 xml = f"https://xmltv.tvkaista.net/guides/sky.com.xml"
 output_xml = "sky.xml"
@@ -131,9 +119,10 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
+
     
-xml = f"https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz"
-output_xml = "epg_ripper_CA1.xml.gz"
+xml = f"https://www.bevy.be/generate/pooVcAFgfH.xml"
+output_xml = "pooVcAFgfH.xml"
 
 try:
     response = requests.get(xml)
@@ -147,8 +136,20 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
+
     
-        # Unzip
-    with zipfile.ZipFile('epg_ripper_CA1.xml.gz', 'r') as zip_ref:
-        zip_ref.extractall()
-    
+xml = f"https://raw.githubusercontent.com/AqFad2811/epg/main/epg.xml"
+output_xml = "aqfad.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
