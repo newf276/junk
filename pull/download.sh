8b62daa1d17c99b0d575d1ffda31b4dde11f77b4
Newf <ian@kellys.me>
2024-06-13 11:08:35 -0700

    Update download.sh

--- pull/download.sh
+++ pull/download.sh
@@ -6,14 +6,6 @@
         wget -O ptv-ca.m3u https://i.mjh.nz/PlutoTV/ca.m3u8
         wget -O ptv-gb.m3u https://i.mjh.nz/PlutoTV/gb.m3u8
         wget -O roku.m3u https://i.mjh.nz/Roku/all.m3u8
-        curl -H "Authorization: token $JAPAN" \
-        -H 'Accept: application/vnd.github.v3.raw' \
-        -o 1.m3u \
-        -L $JP3
-        curl -H "Authorization: token $JAPAN" \
-        -H 'Accept: application/vnd.github.v3.raw' \
-        -o 2.m3u \
-        -L $TWO
         wget -O pluto.xml https://i.mjh.nz/PlutoTV/all.xml
         wget -O roku.xml https://i.mjh.nz/Roku/epg.xml
         wget -O epg_ripper_CA1.xml https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz
