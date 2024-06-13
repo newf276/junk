932a62a448ba05ffa3cb0507d8afa082a80ef573
Newf <ian@kellys.me>
2024-06-12 23:18:25 -0700

    yup again

--- main.py
+++ main.py
@@ -50,3 +50,51 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
+    
+xml = f"https://xmltv.tvkaista.net/guides/ontvtonight.com_ca.xml"
+output_xml = "ontvtonight.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
+
+xml = f"https://xmltv.tvkaista.net/guides/firstmedia.com.xml"
+output_xml = "firstmedia.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
+    
+xml = f"https://xmltv.tvkaista.net/guides/tvhebdo.com.xml"
+output_xml = "tvhebdo.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
