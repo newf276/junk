235678254a7caad2e8db05b25f1efeb28bfae360
Newf <ian@kellys.me>
2024-06-13 13:07:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import zipfile
 
 country = "epg"
 xml = f"https://i.mjh.nz/Roku/{country}.xml"
@@ -130,4 +131,24 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
+    
+xml = f"https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz"
+output_xml = "epg_ripper_CA1.xml.gz"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
+    
+        # Unzip
+    with zipfile.ZipFile('epg_ripper_CA1.xml.gz', 'r') as zip_ref:
+        zip_ref.extractall()
     
