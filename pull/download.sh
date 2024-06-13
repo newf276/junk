253e1c54122c029fc2cbdf259b9767ac205bd4c9
Newf <ian@kellys.me>
2024-06-13 11:21:15 -0700

    yyy

--- pull/download.sh
+++ pull/download.sh
@@ -1,7 +1,7 @@
         sudo apt update
         sudo apt -y install xmltv retry
         go install github.com/aquilax/m3u-combine@latest
-        cd /home/runner/work/newf276/junk/pull
+        cd /newf276/junk/pull
         wget -O epg_ripper_CA1.xml https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz
         gunzip epg_ripper_CA1.xml.gz
         
