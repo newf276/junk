65d8d1918fb8d8341e013499dd88bd4d381898bb
Newf <ian@kellys.me>
2024-06-12 23:27:18 -0700

    more

--- main.py
+++ main.py
@@ -67,6 +67,22 @@ try:
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
 
+xml = f"https://xmltv.tvkaista.net/guides/ontvtonight.com_us.xml"
+output_xml = "ontvtonight_us.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
+    
 xml = f"https://xmltv.tvkaista.net/guides/firstmedia.com.xml"
 output_xml = "firstmedia.xml"
 
