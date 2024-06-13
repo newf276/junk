bc2ea92489cff134cf72664af9c61c81c809e080
Newf <ian@kellys.me>
2024-06-13 08:50:12 -0700

    adjustments

--- main.py
+++ main.py
@@ -114,3 +114,23 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
+    
+xml = f"https://xmltv.tvkaista.net/guides/sky.com.xml"
+output_xml = "sky.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
+    
+    
+    
+    https://xmltv.tvkaista.net/guides/sky.com.xml
