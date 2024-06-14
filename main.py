cd811fa2352e963a3dc10f600249e373788cdfd2
Newf <ian@kellys.me>
2024-06-14 09:29:50 -0700

    complete overhaul

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ except requests.exceptions.RequestException as e:
 
     
 xml = f"https://tinyurl.com/newf276"
-output_xml = "ourguide.xml"
+output_xml = "newf.xml"
 
 try:
     response = requests.get(xml)
@@ -70,59 +70,8 @@ except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
 
     
-xml = f"https://xmltv.tvkaista.net/guides/firstmedia.com.xml"
-output_xml = "firstmedia.xml"
-
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
-
-    
-xml = f"https://xmltv.tvkaista.net/guides/tvhebdo.com.xml"
-output_xml = "tvhebdo.xml"
-
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
-
-    
 xml = f"https://xmltv.tvkaista.net/guides/sky.com.xml"
 output_xml = "sky.xml"
-
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
-
-    
-xml = f"https://www.bevy.be/generate/pooVcAFgfH.xml"
-output_xml = "pooVcAFgfH.xml"
 
 try:
     response = requests.get(xml)
@@ -154,9 +103,9 @@ try:
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
     
-    
-xml = f"https://github.com/acidjesuz/EPGTalk/raw/master/guide.xml"
-output_xml = "acidj.xml"
+
+xml = f"https://raw.githubusercontent.com/dp247/Freeview-EPG/master/epg.xml"
+output_xml = "freeview.xml"
 
 try:
     response = requests.get(xml)
@@ -171,4 +120,7 @@ try:
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
     
+    
 
+        
+        
