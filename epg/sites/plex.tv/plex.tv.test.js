6a422f3af4690a49c1913c4b6723ef9bf7909bc7
Newf <ian@kellys.me>
2024-06-16 17:14:50 -0700

    uopdate

---
+++ epg/sites/plex.tv/plex.tv.test.js
@@ -0,0 +1,56 @@
+const { parser, url, request } = require('./plex.tv.config.js')
+const fs = require('fs')
+const path = require('path')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+dayjs.extend(utc)
+
+jest.mock('axios')
+
+const date = dayjs.utc('2023-02-05', 'YYYY-MM-DD').startOf('d')
+const channel = {
+  site_id: '5e20b730f2f8d5003d739db7-5eea605674085f0040ddc7a6',
+  xmltv_id: 'DarkMatterTV.us'
+}
+
+it('can generate valid url', () => {
+  expect(url({ channel, date })).toBe(
+    'https://epg.provider.plex.tv/grid?channelGridKey=5eea605674085f0040ddc7a6&date=2023-02-05'
+  )
+})
+
+it('can generate valid request headers', () => {
+  expect(request.headers).toMatchObject({
+    'x-plex-provider-version': '5.1'
+  })
+})
+
+it('can parse response', () => {
+  const content = fs.readFileSync(path.resolve(__dirname, '__data__/content.json'))
+  let results = parser({ content })
+  results = results.map(p => {
+    p.start = p.start.toJSON()
+    p.stop = p.stop.toJSON()
+    return p
+  })
+
+  // expect(results.length).toBe(15)
+  expect(results[0]).toMatchObject({
+    start: '2023-02-04T23:31:14.000Z',
+    stop: '2023-02-05T01:10:45.000Z',
+    title: 'Violet & Daisy',
+    description:
+      'Two teenage assassins accept what they think will be a quick-and-easy job, until an unexpected target throws them off their plan.',
+    icon: 'https://provider-static.plex.tv/epg/images/ott_channels/arts/darkmatter-tv-about.jpg',
+    categories: ['Movies']
+  })
+})
+
+it('can handle empty guide', () => {
+  const content = fs.readFileSync(path.resolve(__dirname, '__data__/no_content.json'))
+  const results = parser({ content })
+
+  expect(results).toMatchObject([])
+})
---
+++ epg/sites/tvpassport.com/tvpassport.com.config.js
@@ -0,0 +1,172 @@
+const axios = require('axios')
+const dayjs = require('dayjs')
+const cheerio = require('cheerio')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(customParseFormat)
+
+module.exports = {
+  site: 'tvpassport.com',
+  days: 3,
+  url({ channel, date }) {
+    return `https://www.tvpassport.com/tv-listings/stations/${channel.site_id}/${date.format(
+      'YYYY-MM-DD'
+    )}`
+  },
+  request: {
+    headers: {
+      Cookie: 'cisession=e49ff13191d6875887193cae9e324b44ef85768d;'
+    }
+  },
+  parser: function ({ content }) {
+    let programs = []
+    const items = parseItems(content)
+    for (let item of items) {
+      const $item = cheerio.load(item)
+      const start = parseStart($item)
+      const duration = parseDuration($item)
+      const stop = start.add(duration, 'm')
+      let title = parseTitle($item)
+      let sub_title = parseSubTitle($item)
+      if (title === 'Movie') {
+        title = sub_title
+        sub_title = null
+      }
+
+      programs.push({
+        title,
+        sub_title,
+        description: parseDescription($item),
+        icon: parseIcon($item),
+        category: parseCategory($item),
+        rating: parseRating($item),
+        actors: parseActors($item),
+        guest: parseGuest($item),
+        director: parseDirector($item),
+        start,
+        stop
+      })
+    }
+
+    return programs
+  },
+  async channels() {
+    const xml = await axios
+      .get('https://www.tvpassport.com/sitemap.stations.xml')
+      .then(r => r.data)
+      .catch(console.error)
+
+    let channels = []
+
+    const $ = cheerio.load(xml)
+
+    const elements = $('loc').toArray()
+
+    let total = elements.length
+    let i = 1
+    for (let el of elements) {
+      const url = $(el).text()
+      const [, site_id] = url.match(/\/tv\-listings\/stations\/(.*)$/)
+
+      console.log(`[${i}/${total}]`, url)
+
+      const channelPage = await axios
+        .get(url)
+        .then(r => r.data)
+        .catch(err => console.error(err.message))
+
+      if (!channelPage) continue
+
+      const $channelPage = cheerio.load(channelPage)
+      const title = $channelPage('meta[property="og:title"]').attr('content')
+      const name = title.replace('TV Schedule for ', '')
+
+      channels.push({
+        lang: 'en',
+        site_id,
+        name
+      })
+
+      i++
+    }
+
+    return channels
+  }
+}
+
+function parseDescription($item) {
+  return $item('*').data('description')
+}
+
+function parseIcon($item) {
+  const showpicture = $item('*').data('showpicture')
+  const url = new URL(showpicture, 'https://cdn.tvpassport.com/image/show/960x540/')
+
+  return url.href
+}
+
+function parseTitle($item) {
+  return $item('*').data('showname')
+}
+
+function parseSubTitle($item) {
+  return $item('*').data('episodetitle')
+}
+
+function parseCategory($item) {
+  const showtype = $item('*').data('showtype')
+
+  return showtype ? showtype.split(', ') : []
+}
+
+function parseActors($item) {
+  const cast = $item('*').data('cast')
+
+  return cast ? cast.split(', ') : []
+}
+
+function parseDirector($item) {
+  const director = $item('*').data('director')
+
+  return director ? director.split(', ') : []
+}
+
+function parseGuest($item) {
+  const guest = $item('*').data('guest')
+
+  return guest ? guest.split(', ') : []
+}
+
+function parseRating($item) {
+  const rating = $item('*').data('rating')
+
+  return rating
+    ? {
+        system: 'MPA',
+        value: rating.replace(/^TV/, 'TV-')
+      }
+    : null
+}
+
+function parseStart($item) {
+  const time = $item('*').data('st')
+
+  return dayjs.tz(time, 'YYYY-MM-DD HH:mm:ss', 'America/Vancouver')
+}
+
+function parseDuration($item) {
+  const duration = $item('*').data('duration')
+
+  return parseInt(duration)
+}
+
+function parseItems(content) {
+  if (!content) return []
+  const $ = cheerio.load(content)
+
+  return $('.station-listings .list-group-item').toArray()
+}
