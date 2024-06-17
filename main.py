b1a80714fde626dd77c9ce293d3aa5fb37625f85
Newf <ian@kellys.me>
2024-06-16 17:13:28 -0700

    major

--- main.py
+++ main.py
@@ -86,9 +86,9 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
-    
-xml = f"https://github.com/newf276/xmltv-epg/releases/latest/download/epg-sky.xml"
-output_xml = "sky.xml"
+
+    xml = f"https://tinyurl.com/newf276
+output_xml = "ourguide.xml"
 
 try:
     response = requests.get(xml)
@@ -102,20 +102,5 @@ try:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
-    
-    xml = f"https://github.com/newf276/xmltv-epg/releases/latest/download/epg-tvguide.xml"
-output_xml = "tvguide.xml"
 
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
 
