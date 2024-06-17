4795853c8c663009cd3083c32b8e72a61123c7b6
Newf <ian@kellys.me>
2024-06-16 20:02:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -71,23 +71,7 @@ try:
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
 
-xml = f"https://github.com/newf276/xmltv-epg/releases/latest/download/epg-ontvtonight.xml"
-output_xml = "ontvtonight.xml"
-
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
-
-    xml = f"https://tinyurl.com/newf276
+    xml = f"https://tinyurl.com/newf276"
 output_xml = "ourguide.xml"
 
 try:
