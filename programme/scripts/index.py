4efef1114aec13b93ff488d0517298ec1be76d7e
Newf <ian@kellys.me>
2024-06-17 15:45:15 -0700

    Update index.py

--- programme/scripts/index.py
+++ programme/scripts/index.py
@@ -6,17 +6,20 @@
 import re
 import glob
 import json
+import pgzip
 import shutil
 import logging
 import argparse
+import requests
 import threading
 import platform, os, sys
-
+import lxml.etree as et
 
 parser = argparse.ArgumentParser();
 parser.add_argument('--source', required=True, help='EPG source');
 parser.add_argument('-o', '--output', required=True, help='EPG output');
 parser.add_argument('-t', '--norm-tmp', action='store_true', help='Do not remove temporary files');
+parser.add_argument('-z', '--compress', action='store_true', help='With GZip compressing');
 gen_info = parser.add_argument_group('(Optional) EPG Generated Information');
 gen_info.add_argument('--gen-name', help='Generated name');
 gen_info.add_argument('--gen-url', help='Generated URL');
@@ -24,6 +27,9 @@ args = parser.parse_args();
 
 tmpdir = os.environ['TEMP'] if platform.system() == 'Windows' else ('{}' if os.path.isdir('{}') else '/var{}').format('/tmp');
 tmpdir = tmpdir if os.path.isdir(tmpdir) else os.sep.join(['..', 'tmp']);
+
+
+epg_opt = {'mode': 'wb', 'thread': 0, 'blocksize': 2*10**8} if args.compress else {'mode': 'wb'};
 
 def merge(tree, tagname, attrib):
   print(f'Merging {tagname}...');
