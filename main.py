96b600d4b86a2413b723f50a4163b48b601e53ea
Newf <ian@kellys.me>
2024-06-19 15:10:50 -0700

    scsadfds

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 country = "epg"
 xml = f"https://i.mjh.nz/Roku/{country}.xml"
-output_xml = "programme/rtv.xml"
+output_xml = "rtv.xml"
     
 try:
     response = requests.get(xml)
@@ -18,25 +18,9 @@ except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
     
 
-country = "all"
+country = "ca"
 xml = f"https://github.com/matthuisman/i.mjh.nz/raw/master/PlutoTV/{country}.xml"
-output_xml = "programme/ptv.xml"
-
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
-
-xml = f"https://tinyurl.com/newf276"
-output_xml = "programme/ourguide.xml"
+output_xml = "ptv_ca.xml"
 
 try:
     response = requests.get(xml)
@@ -52,3 +36,20 @@ except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
 
 
+country = "gb"
+xml = f"https://github.com/matthuisman/i.mjh.nz/raw/master/PlutoTV/{country}.xml"
+output_xml = "ptv_gb.xml"
+
+try:
+    response = requests.get(xml)
+    if response.status_code == 200:
+        print("GET request successful.")
+
+        with open(output_xml, "w") as f:
+            f.write(response.text)
+            print(f"Response saved to {output_xml}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print("Error making GET request:", e)
+    
