c801ea0004a9087d7acc5131466ba068a8d521ad
Newf <ian@kellys.me>
2024-06-19 11:36:23 -0700

    tweaks

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
     
 
-country = "ca"
+country = "all"
 xml = f"https://github.com/matthuisman/i.mjh.nz/raw/master/PlutoTV/{country}.xml"
-output_xml = "programme/ptv_ca.xml"
+output_xml = "programme/ptv.xml"
 
 try:
     response = requests.get(xml)
@@ -35,41 +35,6 @@ try:
 except requests.exceptions.RequestException as e:
     print("Error making GET request:", e)
 
-
-country = "gb"
-xml = f"https://github.com/matthuisman/i.mjh.nz/raw/master/PlutoTV/{country}.xml"
-output_xml = "programme/ptv_gb.xml"
-
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
-    
-
-country = "us"
-xml = f"https://github.com/matthuisman/i.mjh.nz/raw/master/PlutoTV/{country}.xml"
-output_xml = "programme/ptv_us.xml"
-
-try:
-    response = requests.get(xml)
-    if response.status_code == 200:
-        print("GET request successful.")
-
-        with open(output_xml, "w") as f:
-            f.write(response.text)
-            print(f"Response saved to {output_xml}")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print("Error making GET request:", e)
 
     xml = f"https://tinyurl.com/newf276"
 output_xml = "programme/ourguide.xml"
