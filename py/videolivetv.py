297a1ee83d4a947c9c8b724d149282669cdec8d5
Newf <ian@kellys.me>
2024-12-12 10:35:17 -0800

    d sds

--- py/videolivetv.py
+++ py/videolivetv.py
@@ -255,7 +255,7 @@ for name, link in live_tv_links:
 
     # Print the collected m3u8 URL
     if m3u8_urls:
-        print(f"#EXTINF:-1 group-title=\"USA TV\" tvg-ID=\"{name}\" tvg-name=\"{name}\" tvg-logo=\"{logo_url}\", {name}")
+        print(f"#EXTINF:-1 tvg-ID=\"{name}\" tvg-name=\"{name}\" tvg-logo=\"{logo_url}\" group-title=\"TVApp\", {name}")
         print(m3u8_url)  # Print only the first m3u8 URL
 
 
