b137b7868b67085ebc567d8051dd7c4300ce005a
Newf <ian@kellys.me>
2024-12-12 22:03:16 -0800

    fdfedbf

--- py/videolivetv.py
+++ py/videolivetv.py
@@ -24,6 +24,7 @@ user_agents = [
     'Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/127.0 Mobile/15E148 Safari/605.1.15',
     'Mozilla/5.0 (Android 14; Mobile; rv:127.0) Gecko/126.0 Firefox/126.0',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:115.0) Gecko/20100101 Firefox/115.0',
+    "okhttp/4.9.3"
 
 ]
 
