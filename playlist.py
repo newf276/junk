7d84f9323bdd28e415cd06399d1b1566d9cb6e78
Newf <ian@kellys.me>
2025-01-18 17:19:29 -0800

    dcwcdw

--- playlist.py
+++ playlist.py
@@ -248,10 +248,10 @@ for name, link in live_tv_links:
         if m3u8_urls:
             m3u8_url = m3u8_urls[0]
         else:
-            m3u8_url = "https://github.com/mikekaprielian/rtnaodhor93n398/raw/main/en/offline.mp4"
+            m3u8_url = "https://github.com/newf276/junk/raw/master/offline.mp4"
     except Exception as e:
         # If an exception occurs (e.g., button not found), use the default link
-        m3u8_url = "https://github.com/mikekaprielian/rtnaodhor93n398/raw/main/en/offline.mp4"
+        m3u8_url = "https://github.com/newf276/junk/raw/master/offline.mp4"
 
     # Print the collected m3u8 URL
     if m3u8_urls:
